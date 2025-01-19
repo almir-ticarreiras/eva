123366002839f5331fe23e924b12d14fafa4856d
Almir Ferreira <ticarreiras@gmail.com>
2025-01-19 19:37:05 -0300

    altera configurações para permitir acesso de todos os hosts ao endpoint [não recomendado]

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-veki@+fahiw%+ry)_ul1)a%ynhkhbloos5#!7trl7h6ll7)!k^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
